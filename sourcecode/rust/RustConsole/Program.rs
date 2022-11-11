4c28a1920738e10df8cd49c4a15b74765f60496c
Daniel Giversen <dibdbg@haderslev.dk>
2022-11-11 09:57:17 +0100

    moved rust to https://github.com/comcf/rustvs/

--- sourcecode/rust/RustConsole/Program.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    trial::public_function();
-
-    //Error! `private_function` is private
-    //trial::private_function();
-
-    trial::indirect_access();
-    println!("Hello, world!");
-}
